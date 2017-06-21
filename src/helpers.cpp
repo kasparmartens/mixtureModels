be92535627e7c4e876ed5cc56bc2d0aef46a5e2a
kasparmartens <kaspar.martens@gmail.com>
2017-06-21 12:07:34 +0100

    bug fix

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,12 +1,12 @@
 #include "global.h"
 
-double logdet_chol_fast(arma::mat L, int D){
-  return 2*sum(L.diag());
+double logdet_chol_fast(arma::mat L){
+  return 2*sum(log(L.diag()));
 }
 
 double loglik_marginal_NIW_fast(int N, int D, double kappa, double nu, arma::mat S_chol, double kappa0, double nu0, arma::mat S0_chol){
   const double pi = 3.141592653589793238462643383280;
-  double res = -0.5*N*D*log(pi) - 0.5*D*(log(kappa) - log(kappa0)) - 0.5*(nu*logdet_chol_fast(S_chol, D) - nu0*logdet_chol_fast(S0_chol, D));
+  double res = -0.5*N*D*log(pi) - 0.5*D*(log(kappa) - log(kappa0)) - 0.5*(nu*logdet_chol_fast(S_chol) - nu0*logdet_chol_fast(S0_chol));
   for(int d=1; d<=D; d++){
     res += Rf_lgammafn(0.5*(nu - d + 1)) - Rf_lgammafn(0.5*(nu0 - d + 1));
   }
